0bafc9f1eecd1b1e6cb7b12d5c9cf81b39185b1a
BharathIndium <92966882+BharathIndium@users.noreply.github.com>
2021-10-26 21:15:24 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,40 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace First
+namespace project2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name;
-            int empid;
-            int phoneno;
-         
-            string email;
-            int salary;
-            name = Console.ReadLine();
-            empid = Convert.ToInt32(Console.ReadLine());
-            phoneno = Convert.ToInt32(Console.ReadLine());
-            email = Console.ReadLine();
+           
 
-
-            DateTime doj = Convert.ToDateTime(Console.ReadLine());
+            DateTime dob = Convert.ToDateTime(Console.ReadLine());
             DateTime today = DateTime.Today;
-            int ex = today.Year - doj.Year;
-            if (doj > today.AddYears(-ex))
-                Console.WriteLine(" experience is" +ex);
-           
-           
-            salary = Convert.ToInt32(Console.ReadLine());
-            salary *= 12;
+            int age = today.Year - dob.Year;
+            if (dob > today.AddYears(-age))
+                age--;
+                Console.WriteLine("age is" + age);
+            if (age<18)
+            {
+                Console.WriteLine(" not eligible");
+            }
+            else
+            {
+                Console.WriteLine("eligible");
+            }
+        
 
-            Console.WriteLine("emp details are:");
-            Console.WriteLine("name is"  + name);
-         
-            //Console.WriteLine("experience is" +experience);
-            Console.WriteLine("annualsalary is"  + salary);
             
+
         }
     }
 }
