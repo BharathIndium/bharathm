2b179939ae499e0adb161b9addddd378daf433d1
BharathIndium <92966882+BharathIndium@users.noreply.github.com>
2021-10-26 21:16:02 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,31 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace project2
+namespace @switch
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+          
            
+         
+            Console.WriteLine("1.Add a detail \n2.update a detail \n3.display detail");
+            Console.WriteLine("Enter the options");
+            int option;
+            option = Convert.ToInt32(Console.ReadLine());
+       
+            string name;
+            int empid;
+            int phoneno;
+            string email;
+            string experience;
+            int salary;
 
-            DateTime dob = Convert.ToDateTime(Console.ReadLine());
-            DateTime today = DateTime.Today;
-            int age = today.Year - dob.Year;
-            if (dob > today.AddYears(-age))
-                age--;
-                Console.WriteLine("age is" + age);
-            if (age<18)
-            {
-                Console.WriteLine(" not eligible");
-            }
-            else
-            {
-                Console.WriteLine("eligible");
-            }
-        
 
-            
+            switch (option)
+            {
+            case 1:
+                Console.WriteLine("Add a detail");
+
+                    name = Console.ReadLine();
+                    empid = Convert.ToInt32(Console.ReadLine());
+                    phoneno = Convert.ToInt32(Console.ReadLine());
+                    email = Console.ReadLine();
+                    experience = Console.ReadLine();
+                    salary = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 2:
+                    Console.WriteLine("update a detail");
+                    Console.WriteLine("given detail are updated");
+                    break;
+                case 3:
+                    Console.WriteLine("emp details are:");
+               
+                    Console.WriteLine ("name=bharath");
+                    Console.WriteLine("empid=1234");
+                    Console.WriteLine("phoneno=12345467890");
+                    Console.WriteLine("emai=bharathmbca@gmail.com");
+                    Console.WriteLine("experience=2years");
+                    Console.WriteLine("salary=100000");
+
+                    break;
+                default :
+                    Console.WriteLine("out of range selection");
+                    break;
+            }
+
 
         }
     }
