dcf8430d4ba5606f2ea95a7b0fdc43b5597ff206
BharathIndium <92966882+BharathIndium@users.noreply.github.com>
2021-10-26 21:21:33 +0530

    Add files via upload

---
+++ task2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            DateTime dob = Convert.ToDateTime(Console.ReadLine());
+            DateTime today = DateTime.Today;
+            int age = today.Year - dob.Year;
+            if (dob > today.AddYears(-age))
+                age--;
+                Console.WriteLine("age is" + age);
+            if (age<18)
+            {
+                Console.WriteLine(" not eligible");
+            }
+            else
+            {
+                Console.WriteLine("eligible");
+            }
+        
+
+            
+
+        }
+    }
+}
