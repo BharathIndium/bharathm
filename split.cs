c79ad60f278a608b8a45333f04eaf98959ac7ca6
BharathIndium <92966882+BharathIndium@users.noreply.github.com>
2021-11-01 09:47:30 +0530

    Add files via upload

---
+++ split.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string student_details;
+            Console.WriteLine("ENTER PARTICIPENT DETAILS");
+            student_details = Console.ReadLine();
+            string[] d = student_details.Split(',');
+
+            foreach (string split in d)
+            {
+                Console.WriteLine(split);
+            }
+        }
+    }
+}
